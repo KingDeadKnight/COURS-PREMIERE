51abda9a0dd2a653f5e7f3b05c654537d1811f40
KingDeadKnight <kingdeadknight@hotmail.com>
2017-12-04 10:02:53 +0100

    Add Etudiant class

---
+++ ConsoleApplication1/Personne.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Etudiant
+    {
+        private string Nom { get; set; }
+        private string _Matricule;
+
+        public string Matricule
+        {
+            get
+            {
+                return this._Matricule;
+            }
+            private set
+            {
+                if (value.ToUpper().StartsWith("LA")) this._Matricule = value;
+                else throw new Exception("Matricule erroné");
+            }
+        }
+
+        public Etudiant(string Nom, string Matricule)
+        {
+            this.Nom = Nom;
+            this.Matricule = Matricule;
+        }
+    }
+}
